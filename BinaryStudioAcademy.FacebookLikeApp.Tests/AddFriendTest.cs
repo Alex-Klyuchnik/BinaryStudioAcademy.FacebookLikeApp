91441762f9567be7bd26f47b386aa7fd2846c70d
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-14 15:27:19 +0300

    Test dummies for AddFriend methods

---
+++ BinaryStudioAcademy.FacebookLikeApp.Tests/AddFriendTest.cs
@@ -0,0 +1,28 @@
+﻿using BinaryStudioAcademy.FacebookLikeApp.Classes;
+using NUnit;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using BinaryStudioAcademy.FacebookLikeApp.Models;
+using NUnit.Framework;
+
+
+namespace BinaryStudioAcademy.FacebookLikeApp.Tests
+{
+    
+    
+    [TestFixture]
+    class AddFriendTests
+    {
+    
+        [Test]
+        public void DeleteTest()
+        {
+           NUnit.Framework.Assert.AreEqual(1,1);
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            NUnit.Framework.Assert.AreEqual(1, 1);
+        }
+    }
+}
