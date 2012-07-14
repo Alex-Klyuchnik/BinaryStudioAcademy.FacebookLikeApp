51009d0800a03a955a5fa29fc79cbc0d94fa87c0
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-14 15:14:21 +0300

    Add/remove friends function implemented

---
+++ BinaryStudioAcademy.FacebookLikeApp/Classes/AddFriend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BinaryStudioAcademy.FacebookLikeApp.Models;
+
+namespace BinaryStudioAcademy.FacebookLikeApp.Classes
+{
+    public class AddFriend 
+    {
+        public void Add (User friend, User user)
+        {
+           user.friends.Add(friend);
+        }
+
+        public void Delete(User friend, User user)
+        {
+            user.friends.Remove(friend);
+        }
+    }
+}
