a89a6b2f7757e57ef5481b5cb95995b07ae6f45c
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-14 16:01:03 +0300

    Dummy suggestion methods
    4 pm checkpoint

---
+++ BinaryStudioAcademy.FacebookLikeApp/Classes/Suggestions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BinaryStudioAcademy.FacebookLikeApp.Models;
+
+namespace BinaryStudioAcademy.FacebookLikeApp.Classes
+{
+    public class Suggestions
+    {
+        public void Accept()
+        {
+             throw new NotImplementedException();
+        }
+
+        public void RefusePermanently()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Renew(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<User> SearchFriendIntersection(User user)
+        {
+             throw new NotImplementedException();
+        }
+    }
+}
